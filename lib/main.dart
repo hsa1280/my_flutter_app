00f85d7316e2cacf952d23517f396011d86a101b
hsa1280 <shianhuang2008@gmail.com>
2020-02-03 14:06:30 +0800

    add networkimage

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,9 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(child: Text(
-        'Hello Shian',
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold
-        ),
-        ),
+      body: Center(child: Image(
+        image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/2013_Fireworks_on_Eiffel_Tower_49.jpg/1200px-2013_Fireworks_on_Eiffel_Tower_49.jpg'),
+        )
       ),
       floatingActionButton: FloatingActionButton(child: Text('click'),onPressed: () {},backgroundColor: Colors.red[600])
     );
