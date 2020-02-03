5363621d556bbee66c091f19802268196db6421b
hsa1280 <shianhuang2008@gmail.com>
2020-02-03 15:05:58 +0800

    use local image

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,7 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(child: Image(
-        image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/2013_Fireworks_on_Eiffel_Tower_49.jpg/1200px-2013_Fireworks_on_Eiffel_Tower_49.jpg'),
-        )
+      body: Center(child: Image.asset('assets/tower.jpg'),
       ),
       floatingActionButton: FloatingActionButton(child: Text('click'),onPressed: () {},backgroundColor: Colors.red[600])
     );
