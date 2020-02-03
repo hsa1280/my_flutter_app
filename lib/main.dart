ae9fb6f5931d002b280ba6ff61de14c5ee1e4e32
hsa1280 <shianhuang2008@gmail.com>
2020-02-03 15:20:54 +0800

    use iconButton

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,13 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(child: Image.asset('assets/tower.jpg'),
+      body: Center(
+        child: IconButton(
+          icon: Icon(Icons.alternate_email), onPressed: () {
+            print('You click me');
+          },
+          color: Colors.amber
+        )
       ),
       floatingActionButton: FloatingActionButton(child: Text('click'),onPressed: () {},backgroundColor: Colors.red[600])
     );
